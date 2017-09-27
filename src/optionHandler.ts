9149e9982cae488ec173396a5f3af0ad54284806
Jeremy Overman <jeremyoverman@gmail.com>
2017-09-26 21:23:21 -0400

    Added comments to OptionHandler

--- src/optionHandler.ts
+++ src/optionHandler.ts
@@ -1,3 +1,9 @@
+/**
+ * Examples:
+ *      -option
+ *      --option
+ *      --option-test123
+ */
 const OPTION_REGEX = /^--?([\w\d-]*)/;
 
 export interface IOptionMap {
@@ -9,6 +15,11 @@ export interface IOptions {
     args: string[];
 }
 
+/**
+ * A class for handling options starting with -- or -
+ * 
+ * @param   optionMap   an option to function mapping
+ */
 export class OptionHandler {
     optionMap: IOptionMap;
     options: IOptions[];
@@ -20,43 +31,64 @@ export class OptionHandler {
         this.hasOptions = Boolean(this.options.length)
     }
 
+    /**
+     * Parse the options and return an array of IOptions
+     */
     parseOptions () {
+        // If the first param isn't an option, don't parse
         if (!this.isNextParamAnOption()) return [];
 
+        // Initialize and empty options array
         let options: IOptions[] = [];
 
+        // Initialize an empty options object
         let current_option: IOptions = {
             option: '',
             args: []
         };
 
         let next_arg;
+
+        // For all parameters
         while ((next_arg = process.argv.shift()) != undefined) {
+            // shift the parameter and see if it's an option
             let match = next_arg.match(OPTION_REGEX);
 
+            // If it is an option
             if (match) {
+                // Check to see if we already have an option stored
                 if (current_option.option) {
+                    // If we do, push it to the options
                     options.push(current_option);
                 }
 
+                // Then create our new option
                 current_option = {
                     option: match[1],
                     args: []
                 };
             } else {
+                // If it's not an options, push it to the current options args
                 current_option.args.push(next_arg);
             }
         } 
 
+        // There will always be one left over options -- push it
         options.push(current_option);
 
         return options;
     }
 
+    /**
+     * Is the next param an option? I.e., does it start with -- or -
+     */
     isNextParamAnOption () {
         return OPTION_REGEX.test(process.argv[0]);
     }
 
+    /**
+     * Run all functions in the option map
+     */
     runAllOptions () {
         for (let i = 0; i < this.options.length; i++) {
             let option = this.options[i];
@@ -64,10 +96,16 @@ export class OptionHandler {
         }
     }
 
+    /**
+     * 
+     * @param option The name of the option
+     * @param args An array of parameters
+     */
     runOption (option: string, args: string[]) {
         if (this.optionMap[option]) {
             this.optionMap[option](args);
         } else {
+            // TODO: replace this with a better helper function
             console.log(`Unkown option "${option}"`);
         }
     }
